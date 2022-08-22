a17a99258d05ee5ca06987a5581a53d74799038e
nelljun <nelljun.kim@gmail.com>
2022-08-22 16:06:16 +0900

    initial commit

--- src/main/java/com/codestates/todoapp/MyController.java
+++ src/main/java/com/codestates/todoapp/MyController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class BasicController {
+public class MyController {
     @GetMapping("/")
     public String basic() {
         return "To-do Application!";
